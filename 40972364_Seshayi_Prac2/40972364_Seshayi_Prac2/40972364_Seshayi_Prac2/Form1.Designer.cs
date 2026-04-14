f338f588003741414e27f5a0bb46c7d437b3635c
Tebatso Seshayi <TebatsoSeshayiOut@outlook.com>
2026-04-14 10:40:44 +0200

    PRAC2

---
+++ 40972364_Seshayi_Prac2/40972364_Seshayi_Prac2/40972364_Seshayi_Prac2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace _40972364_Seshayi_Prac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGV = new System.Windows.Forms.DataGridView();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnShowAllData = new System.Windows.Forms.Button();
+            this.btnMealAndPriceOnly = new System.Windows.Forms.Button();
+            this.btnMealsWithCheaps = new System.Windows.Forms.Button();
+            this.btnMealCostMoreThan150 = new System.Windows.Forms.Button();
+            this.btnSortLowestToLargest = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGV
+            // 
+            this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV.Location = new System.Drawing.Point(12, 12);
+            this.dataGV.Name = "dataGV";
+            this.dataGV.Size = new System.Drawing.Size(531, 426);
+            this.dataGV.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(596, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(111, 51);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "ConnectDB / Clear";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnShowAllData
+            // 
+            this.btnShowAllData.Location = new System.Drawing.Point(596, 69);
+            this.btnShowAllData.Name = "btnShowAllData";
+            this.btnShowAllData.Size = new System.Drawing.Size(111, 51);
+            this.btnShowAllData.TabIndex = 2;
+            this.btnShowAllData.Text = "Show All Data";
+            this.btnShowAllData.UseVisualStyleBackColor = true;
+            this.btnShowAllData.Click += new System.EventHandler(this.btnShowAllData_Click);
+            // 
+            // btnMealAndPriceOnly
+            // 
+            this.btnMealAndPriceOnly.Location = new System.Drawing.Point(596, 136);
+            this.btnMealAndPriceOnly.Name = "btnMealAndPriceOnly";
+            this.btnMealAndPriceOnly.Size = new System.Drawing.Size(111, 51);
+            this.btnMealAndPriceOnly.TabIndex = 3;
+            this.btnMealAndPriceOnly.Text = "Meal And Price Only";
+            this.btnMealAndPriceOnly.UseVisualStyleBackColor = true;
+            this.btnMealAndPriceOnly.Click += new System.EventHandler(this.btnMealAndPriceOnly_Click);
+            // 
+            // btnMealsWithCheaps
+            // 
+            this.btnMealsWithCheaps.Location = new System.Drawing.Point(596, 205);
+            this.btnMealsWithCheaps.Name = "btnMealsWithCheaps";
+            this.btnMealsWithCheaps.Size = new System.Drawing.Size(111, 51);
+            this.btnMealsWithCheaps.TabIndex = 4;
+            this.btnMealsWithCheaps.Text = "Meals With Chips Only";
+            this.btnMealsWithCheaps.UseVisualStyleBackColor = true;
+            this.btnMealsWithCheaps.Click += new System.EventHandler(this.btnMealsWithCheaps_Click);
+            // 
+            // btnMealCostMoreThan150
+            // 
+            this.btnMealCostMoreThan150.Location = new System.Drawing.Point(596, 273);
+            this.btnMealCostMoreThan150.Name = "btnMealCostMoreThan150";
+            this.btnMealCostMoreThan150.Size = new System.Drawing.Size(111, 51);
+            this.btnMealCostMoreThan150.TabIndex = 5;
+            this.btnMealCostMoreThan150.Text = "Meals With Cost More Than 150";
+            this.btnMealCostMoreThan150.UseVisualStyleBackColor = true;
+            this.btnMealCostMoreThan150.Click += new System.EventHandler(this.btnMealCostMoreThan150_Click);
+            // 
+            // btnSortLowestToLargest
+            // 
+            this.btnSortLowestToLargest.Location = new System.Drawing.Point(596, 330);
+            this.btnSortLowestToLargest.Name = "btnSortLowestToLargest";
+            this.btnSortLowestToLargest.Size = new System.Drawing.Size(111, 51);
+            this.btnSortLowestToLargest.TabIndex = 6;
+            this.btnSortLowestToLargest.Text = "Sort By Price Lowest To Largest";
+            this.btnSortLowestToLargest.UseVisualStyleBackColor = true;
+            this.btnSortLowestToLargest.Click += new System.EventHandler(this.btnSortLowestToLargest_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(596, 387);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 51);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSortLowestToLargest);
+            this.Controls.Add(this.btnMealCostMoreThan150);
+            this.Controls.Add(this.btnMealsWithCheaps);
+            this.Controls.Add(this.btnMealAndPriceOnly);
+            this.Controls.Add(this.btnShowAllData);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.dataGV);
+            this.Name = "Form1";
+            this.Text = " Bone’s_Kos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGV;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnShowAllData;
+        private System.Windows.Forms.Button btnMealAndPriceOnly;
+        private System.Windows.Forms.Button btnMealsWithCheaps;
+        private System.Windows.Forms.Button btnMealCostMoreThan150;
+        private System.Windows.Forms.Button btnSortLowestToLargest;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 40972364_Seshayi_Prac2/40972364_Seshayi_Prac2/40972364_Seshayi_Prac2/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _40972364_Seshayi_Prac2
+{
+    public partial class Form1 : Form
+    {
+        // Database connection string
+        //string ConnectionString = "";
+        string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\\143.160.81.13\CTX_Redirected_Data$\40972364\Documents\40972364_Seshayi_Prac2\40972364_Seshayi_Prac2\Database1.mdf;Integrated Security=True";
+       
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataSet dataSet;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize the connection string
+            
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Create a new SqlConnection using the connection string
+            connection = new SqlConnection(ConnectionString);
+            // Empty the data grid view
+            dataGV.DataSource = null;
+
+            // Open the connection
+            connection.Open();
+            // Close the connection
+            connection.Close();
+
+            // Show a message box indicating the connection was successful
+            MessageBox.Show("Connection Successful!!");
+        }
+
+        private void btnShowAllData_Click(object sender, EventArgs e)
+        {
+            // Empty the data grid view
+            dataGV.DataSource = null;
+
+            try
+            {
+                // Using statement to ensure the connection is properly closed
+                using (connection)
+                {
+                    // Open the connection if it's not already open
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    // Initialize the SqlDataAdapter and DataSet
+                    adapter = new SqlDataAdapter();
+                    dataSet = new DataSet();
+
+                    // SQL query to select all data from the BoneKosTable
+                    string SqlString = "SELECT * FROM BoneKosTable";
+
+                    // Initialize the SqlCommand with the SQL query and connection
+                    command = new SqlCommand(SqlString, connection);
+                    adapter.SelectCommand = command;
+
+                    // Fill the DataSet with the data from the BoneKosTable
+                    adapter.Fill(dataSet, "SourceTable");
+
+                    // Set the DataGridView's DataSource to the DataSet
+                    dataGV.DataSource = dataSet;
+                    // Set the DataGridView's DataMember to the table name in the DataSet
+                    dataGV.DataMember = "SourceTable";
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show an error message if an exception occurs
+                MessageBox.Show("Error Displaying: " + ex.Message);
+            }
+        }
+
+        private void btnMealAndPriceOnly_Click(object sender, EventArgs e)
+        {
+            // Empty the data grid view
+            dataGV.DataSource = null;
+
+            try
+            {
+                using (connection)
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    adapter = new SqlDataAdapter();
+                    dataSet = new DataSet();
+
+                    // SQL query to select only the Name and Price columns from the BoneKosTable
+                    string sqlStringMealAndPrice = "SELECT Name, Price FROM BoneKosTable";
+                    command = new SqlCommand(sqlStringMealAndPrice, connection);
+                    adapter.SelectCommand = command;
+                    adapter.Fill(dataSet, "SourceTable");
+                    dataGV.DataSource = dataSet;
+                    dataGV.DataMember = "SourceTable";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Displaying: " + ex.Message);
+            }
+        }
+
+        private void btnMealsWithCheaps_Click(object sender, EventArgs e)
+        {
+            // Empty the data grid view
+            dataGV.DataSource = null;
+
+            try
+            {
+                using (connection)
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    adapter = new SqlDataAdapter();
+                    dataSet = new DataSet();
+
+                    // SQL query to select meals where the Name contains "Chips"
+                    string sqlString = "SELECT * FROM BoneKosTable WHERE Name LIKE '%Chips%'";
+                    command = new SqlCommand(sqlString, connection);
+                    adapter.SelectCommand = command;
+                    adapter.Fill(dataSet, "SourceTable");
+                    dataGV.DataSource = dataSet;
+                    dataGV.DataMember = "SourceTable";
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Displaying: " + ex.Message);
+            }
+        }
+
+        private void btnMealCostMoreThan150_Click(object sender, EventArgs e)
+        {
+            // Empty the data grid view
+            dataGV.DataSource = null;
+
+            try
+            {
+                using (connection)
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    adapter = new SqlDataAdapter();
+                    dataSet = new DataSet();
+
+                    // SQL query to select meals where the Price is greater than 150
+                    string sqlString = "SELECT Name, Price FROM BoneKosTable WHERE Price > 150";
+                    command = new SqlCommand(sqlString, connection);
+                    adapter.SelectCommand = command;
+                    adapter.Fill(dataSet, "SourceTable");
+                    dataGV.DataSource = dataSet;
+                    dataGV.DataMember = "SourceTable";
+
+                    //connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Displaying: " + ex.Message);
+            }
+        }
+
+        private void btnSortLowestToLargest_Click(object sender, EventArgs e)
+        {
+            // Empty the data grid view
+            //connection.Open();
+            dataGV.DataSource = null;
+
+            try
+            {
+                using (connection)
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    adapter = new SqlDataAdapter();
+                    dataSet = new DataSet();
+
+                    // SQL query to select all meals sorted by Price from lowest to highest
+                    string sqlString = "SELECT * FROM BoneKosTable ORDER BY Price";
+                    command = new SqlCommand(sqlString, connection);
+                    adapter.SelectCommand = command;
+                    adapter.Fill(dataSet, "SourceTable");
+                    dataGV.DataSource = dataSet;
+                    dataGV.DataMember = "SourceTable";
+
+                    //connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Displaying: " + ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            Close();
+        }
+    }
+}
+
+
